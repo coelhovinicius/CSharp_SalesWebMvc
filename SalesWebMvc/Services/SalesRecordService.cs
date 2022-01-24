be4ca365fb7ddbb5f34bebc3d193b5fb6e4a8773
coelhovinicius <coelhovinicius@yahoo.com.br>
2022-01-24 20:05:24 -0300

    Implementing grouping search

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -21,7 +21,7 @@ namespace SalesWebMvc.Services
         // Operacao Assincrona - Faz a busca dos registros de venda por data, com datas opcionais
         public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
-            var result = from obj in _context.SalesRecord select obj;
+            var result = from obj in _context.SalesRecord select obj; // Prepara o objeto IQueryable
             if (minDate.HasValue) // Se a data minima for preenchida
             {
                 result = result.Where(x => x.Date >= minDate.Value);// Restricao de data minima para a consulta
@@ -34,6 +34,26 @@ namespace SalesWebMvc.Services
                 .Include(x => x.Seller) // Join entre as tabelas
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        // Agrupa por Departamento
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj; // Prepara o objeto IQueryable
+            if (minDate.HasValue) // Se a data minima for preenchida
+            {
+                result = result.Where(x => x.Date >= minDate.Value);// Restricao de data minima para a consulta
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller) // Join entre as tabelas
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department) // Agrupamento
                 .ToListAsync();
         }
     }
